22c646547faa80ed13c4657a11488bd1f0cfb4db
Mauro Scomparin <scompo@gmail.com>
2020-03-27 18:35:45 +0100

    well

---
+++ public/js/main.js
@@ -0,0 +1,64 @@
+const setupWords = async () => {
+  return fetch('/static/files/parole.txt')
+    .then(r => {
+      if (r.ok) {
+        return r.text()
+      }else {
+        return Promise.resolve("")
+      }
+    })
+    .then(r => r.split("\n"))
+}
+
+const randomWord = async (words) => {
+  return words[Math.floor(Math.random() * words.length)]
+}
+
+const addWord = (stuff, words) => {
+  return (e) => {
+    randomWord(words)
+      .then(word => {
+        let text = stuff.innerText ? stuff.innerText.split(" ") : []
+        text.push(word)
+        stuff.innerText = text.join(" ")
+      })
+  }
+}
+
+const changeWord = (stuff, words) =>  {
+  return (e) => {
+    removeWord(stuff)()
+    addWord(stuff, words)()
+  }
+}
+
+const removeWord = (stuff) => {
+  return (e) => {
+    let text = stuff.innerText ? stuff.innerText.split(" ") : []
+    if (text.length > 0) {
+      text.pop()
+    }
+    stuff.innerText = text.join(" ")
+  }
+}
+
+const setup = async () => {
+  return setupWords()
+    .then(words => {
+      const stuff = document.getElementById('stuff')
+
+      const buttonAdd = document.querySelector('#add')
+      buttonAdd.classList.remove("nascosto")
+      buttonAdd.onclick = addWord(stuff, words)
+
+      const buttonChange = document.getElementById('change')
+      buttonChange.classList.remove("nascosto")
+      buttonChange.onclick = changeWord(stuff, words)
+
+      const buttonRemove = document.getElementById('remove')
+      buttonRemove.classList.remove("nascosto")
+      buttonRemove.onclick = removeWord(stuff)
+    })
+}
+
+window.onload = () => setup();
--- routes/pages/main.mjs
+++ routes/pages/main.mjs
@@ -1,12 +1,13 @@
 'use strict'
 
-import sentences from '../../logic/sentences.mjs'
-
 export default function (fastify, options, done) {
   fastify.get('/', options, (request, reply) => {
-    sentences.getRandom(5)
-      .then(oldSentences => {
-        reply.view('./templates/index.pug', { page: { title: 'index' }, oldSentences: oldSentences })
+    reply.view(
+      './templates/index.pug',
+      {
+        page: {
+          title: 'index'
+        }
       })
   })
 
